2f6d8806bd989a8f8eaa3117096e09de71b09c7b
Moises Nunes <33706215+moisesnunes@users.noreply.github.com>
2026-05-08 16:37:26 -0300

    Refactor input handling for player controls

--- input.cpp
+++ input.cpp
@@ -4,77 +4,96 @@ void processInput(GLFWwindow *window, intel8080 *cpu)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    //if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    //    cpu->port[1] |= 
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+    // if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    //     cpu->port[1] |=
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
         cpu->IOPorts[1] |= 1 << p1Left;
     }
-    else{
+    else
+    {
         cpu->IOPorts[1] &= ~(1 << p1Left);
     }
-    //if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    //    glfwSetWindowShouldClose(window, true);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+    // if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    //     glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
         cpu->IOPorts[1] |= 1 << p1Right;
     }
-    else{
+    else
+    {
         cpu->IOPorts[1] &= ~(1 << p1Right);
     }
-        
-    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS){
+
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+    {
         cpu->IOPorts[1] |= 1 << insertCoin;
     }
-    else{
+    else
+    {
         cpu->IOPorts[1] &= ~(1 << insertCoin);
     }
-        
-    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS){
-         cpu->IOPorts[1] |= 1 << p1Shoot;
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        cpu->IOPorts[1] |= 1 << p1Shoot;
     }
-    else{
+    else
+    {
         cpu->IOPorts[1] &= ~(1 << p1Shoot);
     }
-        
-    if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS){
-         cpu->IOPorts[1] |= 1 << player1Start;
+
+    if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+    {
+        cpu->IOPorts[1] |= 1 << player1Start;
     }
-    else{
+    else
+    {
         cpu->IOPorts[1] &= ~(1 << player1Start);
     }
 
-    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS){
-         cpu->IOPorts[2] |= 1 << tiltControls;
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
+    {
+        cpu->IOPorts[2] |= 1 << tiltControls;
     }
-    else{
+    else
+    {
         cpu->IOPorts[2] &= ~(1 << tiltControls);
     }
 
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
-         cpu->IOPorts[2] |= 1 << player2Left;
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+        cpu->IOPorts[2] |= 1 << player2Left;
     }
-    else{
+    else
+    {
         cpu->IOPorts[2] &= ~(1 << player2Left);
     }
 
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
-         cpu->IOPorts[2] |= 1 << player2Right;
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        cpu->IOPorts[2] |= 1 << player2Right;
     }
-    else{
+    else
+    {
         cpu->IOPorts[2] &= ~(1 << player2Right);
     }
 
-    if (glfwGetKey(window, GLFW_KEY_KP_DECIMAL) == GLFW_PRESS){
-         cpu->IOPorts[2] |= 1 << player2Start;
+    if (glfwGetKey(window, GLFW_KEY_KP_DECIMAL) == GLFW_PRESS)
+    {
+        cpu->IOPorts[2] |= 1 << player2Start;
     }
-    else{
+    else
+    {
         cpu->IOPorts[2] &= ~(1 << player2Start);
     }
 
-    if (glfwGetKey(window, GLFW_KEY_KP_0) == GLFW_PRESS){
-         cpu->IOPorts[2] |= 1 << player2Shoot;
+    if (glfwGetKey(window, GLFW_KEY_KP_0) == GLFW_PRESS)
+    {
+        cpu->IOPorts[2] |= 1 << player2Shoot;
     }
-    else{
+    else
+    {
         cpu->IOPorts[2] &= ~(1 << player2Shoot);
     }
-        
 }
